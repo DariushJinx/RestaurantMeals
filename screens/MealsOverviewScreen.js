c1f9d1e21623fdec1da21d594d0c8816ae27eae4
d.Khazaei <dariushkhazaei629@gmail.com>
2024-05-03 23:48:44 +0330

    handle meals overview screen

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,21 @@
+import { MEALS, CATEGORIES } from "../data/dummy-data";
+import React, { useLayoutEffect } from "react";
+import MealsList from "../components/MealsList/MealsList";
+
+export default function MealsOverviewScreen({ route, navigation }) {
+  const catId = route.params.categoryId;
+
+  const displayMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  useLayoutEffect(() => {
+    const categoryTitle = CATEGORIES.find((category) => category.id === catId).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    });
+  }, [catId, navigation]);
+
+  return <MealsList items={displayMeals} />;
+}
